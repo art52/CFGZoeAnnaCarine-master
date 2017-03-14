ed671d7b64f0a297b13f1de7987ac2a77fa5a8c7
art52 <aanna@hotmail.co.uk>
2017-03-14 11:01:07 +0000

    jquery

--- CFGZoeAnnaCarine-master/script.js
+++ CFGZoeAnnaCarine-master/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-	$('#button').mouseenter(function() {
-		$(this).css('-webkit-box-shadow', '1px 1px rgba(0,0,0,0.5)',
-            '1px 1px rgba(255,255,255,1)');
-		$(this).css('-moz-box-shadow', '1px 1px rgba(0,0,0,0.5)',
-            '1px 1px rgba(255,255,255,1)');
-		$(this).css('box-shadow', '1px 1px rgba(0,0,0,0.5)',
-            '1px 1px rgba(255,255,255,1)');
+	$('.button-text').hover(function() {
+		$(this).fadeOut('slow');
 	});
 });
